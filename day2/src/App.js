388fc0790f8ad7fb91eadcf2e6bbf32c4af3fe0b
chintanrathod08 <chintanrathod2002@gmail.com>
2024-11-19 16:53:51 +0530

    Day2

---
+++ day2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return(
+    <div className='app'>
+      <center><h1> Portfolio</h1> </center>
+      <h1>John Doe</h1>
+    </div>
+  );
+}
+
+export default App;
