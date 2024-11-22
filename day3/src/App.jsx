11bd49adfc9343dcf03ddbb9443b75466588849f
chintanrathod08 <chintanrathod2002@gmail.com>
2024-11-22 12:02:09 +0530

    Day4

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react"
 import './App.css'
 
 function App() {
-  var [name,setstate] = useState("Chintan Rathod")
-  var [grid,setstate] = useState("7303")
-  var [date,setstate] = useState("24-11-2023")
-  var [branch,setstate] = useState("RWR1 (Kalawad Road)")
-  var [course,setstate] = useState("Front End")
-  var [nom,setstate] = useState("9033159452")
-  var [duration,setstate] = useState("12 Month")
+  var [name,setstate] = useState("Chintan Rathod");
+  var [grid,setstate] = useState("7303");
+  var [date,setstate] = useState("24-11-2023");
+  var [branch,setstate] = useState("RWR1 (Kalawad Road)");
+  var [course,setstate] = useState("Front End");
+  var [nom,setstate] = useState("9033159452");
+  var [duration,setstate] = useState("12 Month");
+  
+  
+ 
+
   return (
     <center>
      <div className="idcard">
@@ -20,9 +24,12 @@ function App() {
         <h3>Course Name : {course}</h3>
         <h3>Mobile No. : {nom}</h3>
         <h3>Course Duration : {duration}</h3>
+        
     </div>
     </center>
   );
 }
 
+
 export default App;
+
---
+++ day4/src/counter2.jsx
@@ -0,0 +1,7 @@
+function Counter2(props){
+      return <div>
+       {props.a}
+      </div>
+}
+
+export default Counter2
