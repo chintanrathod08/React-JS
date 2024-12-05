b371c50b76c6ce3585593e9a32530d188dcbf2a9
chintanrathod08 <chintanrathod2002@gmail.com>
2024-12-05 13:41:57 +0530

    day_11

--- day11/src/component/formsubmit.jsx
+++ day11/src/component/formsubmit.jsx
@@ -1,22 +1,18 @@
-
-function Formsubmit(p){
-
-    console.log(p);
-    
-    return(
-        <>
-        <div>
-        {p.arr.map((e)=>{
-            return <div>
-                <img src={e.img} alt="" />
-                <h1>{e.title}</h1>
-                <h4>{e.price}</h4>
-            </div>
-        })}
+function Formsubmit(props) {
+ 
+    return (
+      <>
+      <div>
+      {props.arr.map((e)=>{
+        return <div>
+          <img src={e.img} alt="" />
+          <p>{e.title}</p>
+          <p>{e.price}</p>
         </div>
-        
-        </>
+      })}
+      </div>
+      </>
     )
-}
+  }
 
 export default Formsubmit
